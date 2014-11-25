6f29a4835f936c0434e70bb54dd6d2c2dda7454c
Herover <leon.l.a.nielsen@gmail.com>
2014-11-25 22:14:57 +0100

    ''make it work,,

--- headshatsmasks/flag.lua
+++ headshatsmasks/flag.lua
@@ -9,25 +9,42 @@ local flagFolder = "flags16/"
 local matFlag = nil
 
 function ITEM:OnEquip(ply, modifications) --IsValid(modifications.ccode) andstring.lower(system.GetCountry( ))
-	if file.Exists( "materials/" .. flagFolder .. string.lower(system.GetCountry( )) .. ".png", "GAME" ) then
-		matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
+	if modifications.ccode == nil then
+		modifications.ccode = self:FigureFlag()
+		print("OnEquip: Figure flag" )
 	end
-	--print( modifications.ccode, flagFolder .. string.lower(system.GetCountry( )) .. ".png", IsValid( matFlag ) )
+	print( "OnEquip: " .. modifications.ccode )
+	-- PS:SendModifications(self.ID, modifications)
+end
+
+function ITEM:FigureFlag()
+	if file.Exists( "materials/" .. flagFolder .. string.lower(system.GetCountry( )) .. ".png", "GAME" ) then
+		return string.lower(system.GetCountry( ))
+	else
+		return "europeanunion"
+	end
 end
 
 function ITEM:PostPlayerDraw(ply, modifications, ply2)
-	if matFlag == nil then 
+	if not ply == ply2 then return end
+	if not ply:Alive() then return end
+	if ply.IsSpec and ply:IsSpec() then return end
+	if ply.matFlag == nil then 
 		--if file.Exists( "materials/" .. flagFolder .. modifications.ccode .. ".png", "GAME" ) then
 		--	PS:SendModifications(self.ID, modifications)
 		--	matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
 		--else
-			matFlag = Material( flagFolder .. "europeanunion.png", "" )
+		print("Pre reset: ")
+		PrintTable(modifications)
+		print(".")
+		if modifications.ccode == nil then
+			print("PostPlayerDraw: Figure flag" )
+			modifications.ccode = self:FigureFlag()
+		end
+		ply.matFlag = Material( flagFolder .. modifications.ccode .. ".png", "" )
 		--end
-		print("Something happened, and I had to set flag in PostPlayerDraw")
+		print("Something happened, and I had to set flag in PostPlayerDraw " .. modifications.ccode)
 	end
-	if not ply == ply2 then return end
-	if not ply:Alive() then return end
-	if ply.IsSpec and ply:IsSpec() then return end
 	
 	local offset = Vector(0, 0, 79)
 	local ang = LocalPlayer():EyeAngles()
@@ -37,14 +54,51 @@ function ITEM:PostPlayerDraw(ply, modifications, ply2)
 	ang:RotateAroundAxis(ang:Right(), 90)
 	
 	cam.Start3D2D(pos, Angle(0, ang.y, 90), 0.1)
-		surface.SetMaterial( matFlag )
+		surface.SetMaterial( ply.matFlag )
 		surface.DrawTexturedRect(-25, 50, 50, 50 * flagAsp)
 	cam.End3D2D()
 end
 
+function ITEM:PlayerSpawn(ply)
+	ply.matFlag = nil
+    ply:PS_Notify('You have respawned!')
+end
+
 function ITEM:Modify(modifications)
-	PrintTable(modifications)
-	modifications.ccode = string.lower(system.GetCountry( ))
-	PS:SendModifications(self.ID, modifications)
-	matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
+	local FlagPanel = vgui.Create( "DPanel" )
+	FlagPanel:SetPos( 10, 30 ) -- Set the position of the panel
+	FlagPanel:SetSize( 200, 200 ) -- Set the size of the panel
+
+	local FlagLabel = vgui.Create( "DLabel", FlagPanel )
+	FlagLabel:SetPos( 10, 10 ) -- Set the position of the label
+	FlagLabel:SetText( "Select a flag" ) -- Set the text of the label
+	FlagLabel:SizeToContents() -- Size the label to fit the text in it
+	FlagLabel:SetDark( 1 ) -- Set the colour of the text inside the label to a darker one
+	local FlagHolder = vgui.Create( "DGrid", FlagPanel )
+	FlagHolder:SetPos( 10, 30 )
+	FlagHolder:SetSize( 180, 180 )
+	FlagHolder:SetColWide( 32 )
+	local files, directories = file.Find( "materials/" .. flagFolder .. "*", "GAME" )
+	for k, v in pairs(files) do 
+		local FlagImg = vgui.Create( "DImageButton" )
+		FlagImg:SetSize( 32, 32*flagAsp )
+		FlagImg:SetImage( flagFolder .. v )
+		FlagImg.DoClick = function()
+			modifications.ccode = string.Split(v, ".")[1]
+			print( "Click: " .. v .. modifications.ccode)
+			PS:SendModifications(self.ID, modifications)
+			ply.matFlag = nil
+			FlagPanel:Remove()
+		end
+		print( flagFolder .. v )
+		FlagHolder:AddItem( FlagImg )
+	end
+	
+	--PrintTable(modifications)
+	--if not IsValid(modifications.ccode) then
+	--	modifications.ccflag = self:FigureFlag()
+	--end
+	--print( modifications.ccode )
+	--PS:SendModifications(self.ID, modifications)
+	--matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
 end
