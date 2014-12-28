3877edc38cba9be03a886d0eff38524a4b664967
Herover <leon.l.a.nielsen@gmail.com>
2014-12-28 19:47:58 +0100

    Removed spawnmessage, new way to figure if flag is valid

--- flags/flag.lua
+++ flags/flag.lua
@@ -6,44 +6,56 @@ ITEM.NoPreview = true
 local flagAsp = 11/16 -- Most flags seem to have theese proportions
                       -- TODO: figure this out dynamically
 local flagFolder = "flags16/"
-local matFlag = nil
+local defaultFlag = "europeanunion"
 
-function ITEM:OnEquip(ply, modifications) --IsValid(modifications.ccode) andstring.lower(system.GetCountry( ))
-	--if modifications.ccode == nil then
-	--	modifications.ccode = self:FigureFlag()
-	--	print("OnEquip: Figure flag" )
-	--end
-	--print( "OnEquip: " .. modifications.ccode )
-	-- PS:SendModifications(self.ID, modifications)
+function ITEM:OnEquip(ply, modifications)
+	if modifications.ccode == nil then
+		modifications.ccode = defaultFlag
+	end
 end
 
 function ITEM:FigureFlag()
 	if file.Exists( "materials/" .. flagFolder .. string.lower(system.GetCountry( )) .. ".png", "GAME" ) then
 		return string.lower(system.GetCountry( ))
 	else
-		return "europeanunion"
+		return defaultFlag
 	end
 end
+
+--function ITEM:PlayerSpawn(ply, modifications)
+--	print("spawn")
+--	ply.matFlag = nil
+--end
 
 function ITEM:PostPlayerDraw(ply, modifications, ply2)
 	if not ply == ply2 then return end
 	if not ply:Alive() then return end
 	if ply.IsSpec and ply:IsSpec() then return end
-	if ply.matFlag == nil or !string.find(ply.matFlag:GetName(), modifications.ccode .. ".png") then 
-		--if file.Exists( "materials/" .. flagFolder .. modifications.ccode .. ".png", "GAME" ) then
-		--	PS:SendModifications(self.ID, modifications)
-		--	matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
-		--else
+	if ply.matFlag == nil or modifications.ccode == nil or !(ply.matFlag:GetName() == flagFolder .. modifications.ccode) then 
 		print("Pre reset: ")
 		PrintTable(modifications)
-		print(".")
+		if ply.matFlag == nil then print("matFlag is nil")
+		else print("Invalidated flag")
+		end
 		if modifications.ccode == nil then
-			print("PostPlayerDraw: Figure flag" )
-			modifications.ccode = self:FigureFlag()
+			print("PostPlayerDraw: Figure flag -- THIS IS BAD" )
+			modifications.ccode = defaultFlag
 		end
 		ply.matFlag = Material( flagFolder .. modifications.ccode .. ".png", "" )
+		ply.FlagCC = modifications.ccode
 		--end
-		print("Something happened, and I had to set flag in PostPlayerDraw " .. modifications.ccode)
+		print("Flag is " .. ply.matFlag:GetName())
+		
+
+		for k, v in pairs( player.GetAll() ) do
+			print("FlagHat: Invalidating no." .. k)
+			--v.matFlag = nil
+			if !v.FlagCC == nil then
+				p.matFlag = Material( flagFolder .. p.FlagCC .. ".png", "" )
+				print( v.FlagCC )
+			end
+		end
+		
 	end
 	
 	local offset = Vector(0, 0, 79)
@@ -55,50 +67,38 @@ function ITEM:PostPlayerDraw(ply, modifications, ply2)
 	
 	cam.Start3D2D(pos, Angle(0, ang.y, 90), 0.1)
 		surface.SetMaterial( ply.matFlag )
-		surface.DrawTexturedRect(-25, 50, 50, 50 * flagAsp)
+		surface.SetDrawColor( 255, 255, 255, 200 )
+		surface.DrawTexturedRect(-25, 50, 175, 175 * flagAsp)
 	cam.End3D2D()
-end
-
-function ITEM:PlayerSpawn(ply)
-	ply.matFlag = nil
-    ply:PS_Notify('You have respawned!')
 end
 
 function ITEM:Modify(modifications)
 	local FlagPanel = vgui.Create( "DPanel" )
-	FlagPanel:SetPos( 10, 30 ) -- Set the position of the panel
-	FlagPanel:SetSize( 200, 200 ) -- Set the size of the panel
+	FlagPanel:SetPos( ScrW( )/2 - 50, 30 )
+	FlagPanel:SetSize( 200, 220 )
 
 	local FlagLabel = vgui.Create( "DLabel", FlagPanel )
-	FlagLabel:SetPos( 10, 10 ) -- Set the position of the label
-	FlagLabel:SetText( "Select a flag" ) -- Set the text of the label
-	FlagLabel:SizeToContents() -- Size the label to fit the text in it
+	FlagLabel:SetPos( 10, 10 )
+	FlagLabel:SetText( "Select a flag" )
+	FlagLabel:SizeToContents()
 	FlagLabel:SetDark( 1 ) -- Set the colour of the text inside the label to a darker one
-	local FlagHolder = vgui.Create( "DGrid", FlagPanel )
+	local FlagHolder = vgui.Create( "DScrollPanel", FlagPanel )
 	FlagHolder:SetPos( 10, 30 )
 	FlagHolder:SetSize( 180, 180 )
-	FlagHolder:SetColWide( 32 )
-	FlagHolder:SetVerticalScrollbarEnabled( ) 
 	local files, directories = file.Find( "materials/" .. flagFolder .. "*", "GAME" )
+	local flagW = 32
+	local flagH = 32*flagAsp
+	local eachRow = 5
 	for k, v in pairs(files) do 
-		local FlagImg = vgui.Create( "DImageButton" )
-		FlagImg:SetSize( 32, 32*flagAsp )
+		local FlagImg = vgui.Create( "DImageButton", FlagHolder )
+		FlagImg:SetPos((k % eachRow) * flagW, math.floor(k/eachRow) * flagH)
+		FlagImg:SetSize( flagW, flagH )
 		FlagImg:SetImage( flagFolder .. v )
 		FlagImg.DoClick = function()
 			modifications.ccode = string.Split(v, ".")[1]
 			print( "Click: " .. v .. modifications.ccode)
 			PS:SendModifications(self.ID, modifications)
-			--ply.matFlag = nil
 			FlagPanel:Remove()
 		end
-		FlagHolder:AddItem( FlagImg )
 	end
-	
-	--PrintTable(modifications)
-	--if not IsValid(modifications.ccode) then
-	--	modifications.ccflag = self:FigureFlag()
-	--end
-	--print( modifications.ccode )
-	--PS:SendModifications(self.ID, modifications)
-	--matFlag = Material( flagFolder .. string.lower(system.GetCountry( )) .. ".png", "" )
 end
